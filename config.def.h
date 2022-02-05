0258caafbd39f023d1e56a7bb9e6c2ea1b37c19c
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2022-02-05 11:44:38 -0500

    Fixed a typo in `config.def.h`.

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+/* Default settings; can be overridden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
